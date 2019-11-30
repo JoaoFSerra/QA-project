c7f3eb5a59262024e5026790e5035cd69a40339e
Joao <joao@localhost.localdomain>
2019-11-30 16:28:39 +0200

    loginpage

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,17 +1,15 @@
 import React from "react";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Container from "@material-ui/core/Container";
-import Questions from "./Questions";
+import Login from "./Login";
 
 export default function Main() {
   return (
     <React.Fragment>
       <CssBaseline />
-      <Container
-        maxWidth="md"
-        style={{ backgroundColor: "pink", height: "100vh" }}
-      >
-        <Questions />
+      <Container>
+       
+        <Login />
       </Container>
     </React.Fragment>
   );
